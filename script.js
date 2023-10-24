ce17d5c3467271ff9307ade9242ba00a5daac559
Farhad-Rahimi-KLIE <farhadrahimiklie786@gmail.com>
2023-10-24 06:21:11 +0430

    All files was uploaded

---
+++ script.js
@@ -0,0 +1,18 @@
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "YOUR-API-KEY"
+    let email = document.getElementById("username").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+    console.log(str)
+    resultCont.innerHTML = str
+})
